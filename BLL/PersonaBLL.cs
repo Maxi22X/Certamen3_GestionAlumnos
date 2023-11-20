1d40e7e783c75716a28e2cc489fb5fa481695916
Maxi22X <maxsef25@hotmail.com>
2023-11-20 17:18:42 -0300

    Se agrega primera parte del certamen 3

---
+++ BLL/PersonaBLL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DAL;
+namespace BLL
+{
+    public class PersonaBLL
+    {
+        public void AgregarPersona(string run, string nombre, string apellido,
+            DateTime fechaNacimiento, string direccion, string estadoCivil)
+        {
+            using(MasterDbContext db = new MasterDbContext())
+            { 
+               Persona persona = new Persona();
+                persona.Run = run;
+                persona.Nombre = nombre;
+                persona.Apellido = apellido;
+                persona.FechaNacimiento = fechaNacimiento;
+                persona.Direccion = direccion;
+                persona.EstadoCivil = estadoCivil;
+
+                db.Persona.Add(persona);
+
+                db.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ UI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL;
+
+namespace UI
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            PersonaBLL persona = new PersonaBLL();
+            Console.WriteLine("Ingrese una nueva persona");
+           
+        }
+    }
+}
